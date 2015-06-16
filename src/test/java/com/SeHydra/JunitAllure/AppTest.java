3bfb6aa7d8ff11ec565c1840c12f3c1d54a30759
ccbmonks <conallbennett@hotmail.com>
2015-06-17 00:13:59 +0100

    Initial commit

---
+++ src/test/java/com/SeHydra/JunitAllure/AppTest.java
@@ -0,0 +1,17 @@
+package com.SeHydra.JunitAllure;
+
+import org.junit.Test;
+import static junit.framework.Assert.assertTrue;
+
+public class AppTest 
+
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue(true);
+    }
+}
